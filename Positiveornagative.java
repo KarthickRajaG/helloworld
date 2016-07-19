8330e70ae9bd5d0463b71a31e653112ff03026c5
KarthickRajaG <gkarthickrajag@gmail.com>
2016-07-19 22:24:50 +0530

    positiveornagative.java

---
+++ Positiveornagative.java
@@ -0,0 +1,22 @@
+public class Positiveornegative {
+
+ 
+
+    public void check(int number) {
+
+        if (number > 0) {
+
+            System.out.println(number + " is a positive number");
+
+        } else if (number < 0) {
+
+            System.out.println(number + " is a negative number");
+
+      } else {
+
+            System.out.println(number + " is neither positive nor negative");
+        }
+
+    }
+
+}
