4ee2eb3e1e5a04e44d6045acbf7eb2dffe10804a
KarthickRajaG <gkarthickrajag@gmail.com>
2016-07-21 19:21:33 +0530

    Prefix.java

---
+++ Prefix.java
@@ -0,0 +1,23 @@
+Class power{
+Public String longestCommonPrefix(String[] strs) {
+ String longestPrefix = ""; 
+if(strs.length>0){ 
+longestPrefix = strs[0]; 
+} for(int i=1; i<strs.length; i++){ 
+String analyzing = strs[i];
+ for(into j=0; j<Math.min(longestPrefix.length(), strs[i].length()); j++)
+{
+ if(longestPrefix.charAt(j) != analyzing.charAt(j)){ 
+break; 
+} } 
+longestPrefix = strs[i].substring(0, j);
+ } 
+return longestPrefix; 
+}
+Public class Prefix{
+Public static void main(String args[])
+{
+String str="http_array hati_yuuag";
+power p=new power();
+p.longestComminPrefix(str);
+}}
